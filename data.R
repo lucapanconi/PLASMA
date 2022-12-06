8fed640eb8ee0ba317e6bd5d86471bdcc06b3a35
Luca Panconi (PhD Integ St Topolog Design FT) <LXP609@student.bham.ac.uk>
2022-12-06 17:24:03 +0000

    Add data

---
+++ data.R
@@ -0,0 +1,8 @@
+#' @title Simulated marked point patterns.
+#'
+#' @description A list of 2000 simulated marked point patterns.
+#'
+#' @format A list containing several data frames.
+#'
+#' @source <https://www.github.com/lucapanconi/PLASMA>
+"Simulations"
